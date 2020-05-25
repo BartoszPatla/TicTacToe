fc3f871f4078594f2cde181e9ca1510590970d8e
BartoszPatla <60610411+BartoszPatla@users.noreply.github.com>
2020-05-25 13:53:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
 class Game{
     constructor(){
-        this.fields = document.querySelectorAll('.field')
-        this.fields.forEach(field => {
-            field.addEventListener('click' , this.handleFieldClick.bind(this))
-
-        })
-    }
-    handleFieldClick(event){
-        const index = event.target.id;
-        console.log(index);
-
-    }
-}
-window.addEventListener('load' , () => {
-    const game = new Game();
-    
-
-}
+ 
